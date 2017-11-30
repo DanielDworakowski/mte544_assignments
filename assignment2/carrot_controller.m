03ab0dc5fc629d5d5c1e5ff5c92bfabb1786eafb
Daniel Dworakowski <daniel@Daniels-MacBook-Pro.local>
2017-11-30 00:46:43 -0500

    map

--- assignment2/carrot_controller.m
+++ assignment2/carrot_controller.m
@@ -27,36 +27,36 @@ v=k*norm(s);
 if(norm(p-p1) < tol)
   done = 1;
 end
-disp(["velocity: ", num2str(v), "   delta: ", num2str(delta), "    done: ", num2str(done)]);
+% disp(["velocity: ", num2str(v), "   delta: ", num2str(delta), "    done: ", num2str(done)]);
 
 sz = 200;
-hold
-axis([-5, 5, -5, 5]);
-scatter(p0(1),p0(2), sz,1,'filled');
-text(p0(1)-0.1,p0(2)-0.1, 'p0');
-
-scatter(e(1),e(2), sz,8,'filled');
-text(e(1)-0.1,e(2)-0.1, 'e');
-
-scatter(pc(1),pc(2), sz,7,'filled');
-text(pc(1)-0.1,pc(2)-0.1, 'pc');
-
-scatter(p1(1),p1(2), sz,2,'filled');
-text(p1(1)-0.1,p1(2)-0.1, 'p1');
-
-scatter(p(1),p(2), sz,3,'filled');
-text(p(1)-0.1,p(2)-0.1, 'p');
-
-scatter(rv(1),rv(2), sz,8,'filled');
-text(rv(1)-0.1,rv(2)-0.1, 'rv');
-
-scatter(p(1)+0.1*cos(theta),p(2)+0.1*sin(theta), sz,3,'filled');
-text(p(1)+0.1*cos(theta)-0.1,p(2)+0.1*sin(theta)-0.1, 'w');
-
-scatter(s(1),s(2), sz,4,'filled');
-text(s(1)-0.1,s(2)-0.1, 's');
-
-scatter(0,0, sz,4,'filled');
-text(-0.1,-0.1, 'O');
+% hold
+% axis([-5, 5, -5, 5]);
+% scatter(p0(1),p0(2), sz,1,'filled');
+% text(p0(1)-0.1,p0(2)-0.1, 'p0');
+% 
+% scatter(e(1),e(2), sz,8,'filled');
+% text(e(1)-0.1,e(2)-0.1, 'e');
+% 
+% scatter(pc(1),pc(2), sz,7,'filled');
+% text(pc(1)-0.1,pc(2)-0.1, 'pc');
+% 
+% scatter(p1(1),p1(2), sz,2,'filled');
+% text(p1(1)-0.1,p1(2)-0.1, 'p1');
+% 
+% scatter(p(1),p(2), sz,3,'filled');
+% text(p(1)-0.1,p(2)-0.1, 'p');
+% 
+% scatter(rv(1),rv(2), sz,8,'filled');
+% text(rv(1)-0.1,rv(2)-0.1, 'rv');
+% 
+% scatter(p(1)+0.1*cos(theta),p(2)+0.1*sin(theta), sz,3,'filled');
+% text(p(1)+0.1*cos(theta)-0.1,p(2)+0.1*sin(theta)-0.1, 'w');
+% 
+% scatter(s(1),s(2), sz,4,'filled');
+% text(s(1)-0.1,s(2)-0.1, 's');
+% 
+% scatter(0,0, sz,4,'filled');
+% text(-0.1,-0.1, 'O');
 end
 
--- assignment2/model.m
+++ assignment2/model.m
@@ -7,10 +7,108 @@ init = zeros(3,1);
 v = ones(1, T/dt + 1) * 3;
 delta = (10 - ts) .* pi / 180.;
 X = motionModel(v, delta, nan);
-plotState(X, 'q1MotModel');
+plotState(X, true, 'q1MotModel');
+%% Question 3.
+clear
+[map, start, goal, res, xMax, yMax] = getMap(false);
 
-function [] = plotState(X, name)
-  figure;
+
+
+
+tic;
+
+% Get milestones
+nS = 1000;
+% 
+% Max sample locations
+sampleMaxX = xMax - 1;
+sampleMaxY = yMax - 1;
+start(1:2) = start(1:2) / res;
+goal(1:2) = goal(1:2) / res;
+samples = round([sampleMaxX*rand(nS,1) + 1, sampleMaxY*rand(nS,1) + 1]);
+idx = sub2ind(size(map),samples(:,1), samples(:,2));
+keep = ~map(idx);
+milestones = [start(1:2); goal; samples(find(keep==1),:)];
+% 
+% Plot samples.
+figure(1); clf; hold on;
+colormap('gray');
+imagesc(1-map');
+plot(start(1), start(2), 'ro', 'MarkerSize',10, 'LineWidth', 3);
+plot(goal(1), goal(2), 'gx', 'MarkerSize',10, 'LineWidth', 3 );
+axis equal
+plot(samples(:,1),samples(:,2),'k.');
+plot(milestones(:,1),milestones(:,2),'m.');
+nM = length(milestones(:,1));
+disp('Time to generate milestones');
+toc;
+
+% Attempt to add closest p edges
+tic;
+p = 20;
+e = zeros(nM,nM);
+D = zeros*ones(nM,nM);
+for i = 1:nM
+  % Find closest neighbours
+  for j = 1:nM
+      d(j) = norm(milestones(i,:)-milestones(j,:));
+  end
+  [d2,ind] = sort(d);
+  % Check for edge collisions (no need to check if entire edge is
+  % contained in obstacles as both endpoints are in free space)
+  for j=1:p
+    cur = ind(j);
+    if (i<cur)
+       x1 = milestones(i,1);
+       y1 = milestones(i,2);
+       x2 = milestones(cur,1);
+       y2 = milestones(cur,2);
+       [x, y] = bresenham(x1, y1, x2, y2);
+       idx = sub2ind(size(map), x, y);
+       isCollided = any(map(idx));
+       if (~isCollided)
+         e(i, cur) = 1;
+         e(cur, i) = 1;
+       end
+       
+%       if (~CheckCollision(milestones(i,:),milestones(cur,:), obsEdges))
+%           e(i,cur) = 1;
+%           e(cur,i) = 1;
+%           plot([milestones(i,1) milestones(cur,1)],[milestones(i,2) milestones(cur,2)],'m');
+%       end
+    end
+  end
+end
+disp('Time to connect roadmap');
+toc;
+
+% Find shortest path
+tic;
+[sp, sd] = shortestpath(milestones, e, 1, 2);
+for i=1:length(sp)-1
+    plot(milestones(sp(i:i+1),1),milestones(sp(i:i+1),2), 'go-', 'LineWidth',3);
+end
+disp('Time to find shortest path');
+toc;
+
+pose = start;
+poses = [];
+for i=1:length(milestones)-1
+  start_m = [milestones(i,1), milestones(i,2)];
+  goal_m = [milestones(i+1,1), milestones(i+1,2)];
+  done = 0;
+  while ~done
+    [ v, delta, done ] = carrot_controller(start_m, goal_m, pose);
+    pose = motionModel(v, delta, pose);
+    poses = [poses pose];
+  end
+end
+plotState(poses,false, "wut");
+
+function [] = plotState(X, newFig, name)
+  if (newFig)
+    figure;
+  end
   plot(X(:,1), X(:,2));
   title('State x vs y position');
   ylabel('y (m)');
@@ -63,4 +161,71 @@ function [X] = motionModel(vel, delta, init)
     prevState = X(index, :);
   end
 
+end
+
+function [map, start, goal, res, xMax, yMax] = getMap(plot)
+  I = imread('IGVCmap.jpg');
+  map = im2bw(I, 0.7); % Convert to 0-1 image
+  map = 1-flipud(map)'; % Convert to 0 free, 1 occupied and flip.
+  [xMax,yMax]= size(map); % Map size
+
+  % Robot start position
+  res = 0.1;
+  start = [40 5 pi];
+
+  % Target location
+  goal = [50 10];
+
+  % Plotting
+  if plot
+    figure(1); clf; hold on;
+    colormap('gray');
+    imagesc(1-map');
+    plot(start(1)/res, start(2)/res, 'ro', 'MarkerSize',10, 'LineWidth', 3);
+    plot(goal(1)/res, goal(2)/res, 'gx', 'MarkerSize',10, 'LineWidth', 3 );
+    axis equal
+  end
+end
+
+% 
+% From MTE544 git.
+function [x y]=bresenham(x1,y1,x2,y2)
+
+%Matlab optmized version of Bresenham line algorithm. No loops.
+%Format:
+%               [x y]=bham(x1,y1,x2,y2)
+%
+%Input:
+%               (x1,y1): Start position
+%               (x2,y2): End position
+%
+%Output:
+%               x y: the line coordinates from (x1,y1) to (x2,y2)
+%
+%Usage example:
+%               [x y]=bham(1,1, 10,-5);
+%               plot(x,y,'or');
+x1=round(x1); x2=round(x2);
+y1=round(y1); y2=round(y2);
+dx=abs(x2-x1);
+dy=abs(y2-y1);
+steep=abs(dy)>abs(dx);
+if steep t=dx;dx=dy;dy=t; end
+
+%The main algorithm goes here.
+if dy==0 
+    q=zeros(dx+1,1);
+else
+    q=[0;diff(mod([floor(dx/2):-dy:-dy*dx+floor(dx/2)]',dx))>=0];
+end
+
+%and ends here.
+
+if steep
+    if y1<=y2 y=[y1:y2]'; else y=[y1:-1:y2]'; end
+    if x1<=x2 x=x1+cumsum(q);else x=x1-cumsum(q); end
+else
+    if x1<=x2 x=[x1:x2]'; else x=[x1:-1:x2]'; end
+    if y1<=y2 y=y1+cumsum(q);else y=y1-cumsum(q); end
+end
 end
