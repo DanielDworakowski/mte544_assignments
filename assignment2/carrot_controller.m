1357663cc976d8e043df9f03039a3fbdffe85e98
Rae <raychanjeong@gmail.com>
2017-12-02 10:55:02 -0500

    finfin

--- assignment2/carrot_controller.m
+++ assignment2/carrot_controller.m
@@ -1,22 +1,71 @@
 function [ v, delta, done ] = carrot_controller(start, goal, pose)
-delta_max = deg2rad(30);
-velocity = 3;
-k = 4;
-tol = 0.35;
+r = 1.9;
+k = 1.0;
+tol = 0.9;
 done = 0;
-position = [pose(1) pose(2)];
-end_point = goal;
-start_point = start;
-traj_angle = atan2(end_point(2) - start_point(2), end_point(1) - start_point(1));
+delta_max = deg2rad(30);
 
-[crosstrack_error, next_point] = distanceToLineSegment(start_point,end_point,position);
+p0 = start;
+p1 = goal;
+p = [pose(1), pose(2)];
+theta = pose(3);
 
-% Calculate steering angle
-delta = max(-delta_max,min(delta_max, angleWrap(traj_angle - pose(3))+ atan2(-k*crosstrack_error,velocity)));
-v = velocity;
+e = (p1-p0);
 
-if(norm(position-goal) < tol)
+t0 = dot(e, p - p0) / dot(e, e);
+
+px = p0 + t0*e;
+
+rv = px + e;
+
+s = rv - p;
+
+delta = max(-delta_max,min(delta_max, angleWrap(k*(atan2(s(2),s(1))) - (theta))));
+v=3;
+% dif = p1-p0;
+% if delta > deg2rad(30)
+%   delta = deg2rad(30);
+% %    v=2.2;
+% end
+% if delta < -deg2rad(30)
+%   delta = -deg2rad(30);
+% %    v=2.2;
+% end
+if(norm(p-p1) < tol)
   done = 1;
 end
+% disp(["velocity: ", num2str(v), "   delta: ", num2str(delta), "    done: ", num2str(done)]);
+% pose
+% sz = 20;
+% figure
+% hold
+% axis([-100, 100, -100, 100]);
+% scatter(p0(1),p0(2), sz,1,'filled');
+% text(p0(1)-0.1,p0(2)-0.1, 'p0');
+% 
+% scatter(e(1),e(2), sz,8,'filled');
+% text(e(1)-0.1,e(2)-0.1, 'e');
+% 
+% scatter(pc(1),pc(2), sz,7,'filled');
+% text(pc(1)-0.1,pc(2)-0.1, 'pc');
+% 
+% scatter(p1(1),p1(2), sz,2,'filled');
+% text(p1(1)-0.1,p1(2)-0.1, 'p1');
+% 
+% scatter(p(1),p(2), sz,3,'filled');
+% text(p(1)-0.1,p(2)-0.1, 'p');
+% 
+% scatter(rv(1),rv(2), sz,8,'filled');
+% text(rv(1)-0.1,rv(2)-0.1, 'rv');
+% 
+% scatter(p(1)+0.1*cos(theta),p(2)+0.1*sin(theta), sz,3,'filled');
+% text(p(1)+0.1*cos(theta)-0.1,p(2)+0.1*sin(theta)-0.1, 'w');
+% 
+% scatter(s(1),s(2), sz,4,'filled');
+% text(s(1)-0.1,s(2)-0.1, 's');
+% 
+% scatter(0,0, sz,4,'filled');
+% text(-0.1,-0.1, 'O');
+
 end
 
